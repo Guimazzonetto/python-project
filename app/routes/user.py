2eddf086ac3fdcfa35586011f1f4c42bfb7300e6
GuilhermeRodriguesDigi <guilherme.rodrigues@digisystem.com.br>
2025-11-10 10:58:21 -0300

    feature: update responses

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import JSONResponse
+
 from  ..database.schema import User
 from ..core.database import users_collection
 from ..core.logging import logger
@@ -18,24 +20,54 @@ async def create_user(user: User):
         # Faz com que o Schema seja entregue como JSON
         users_collection.insert_one(user.model_dump(by_alias=True))
         logger.info("User saved with success")
+        return JSONResponse(
+          content={"message": "user created successfully"},
+          status_code=201
+        )
       
       except Exception as e:
         logger.exception(f'Error while save new user in database: {e}')
+        return JSONResponse(
+          content={"message": "Error while saving user"},
+          status_code=500
+        )
     
     else:
       logger.info(f'User exists')
+      return JSONResponse(
+        content={"message": "User already existis"},
+        status_code=409
+      )
 
   except Exception as e:
-    logger.exception(f'Error while get user in database {e}')
-
-  return user
-
+    logger.exception(f'Error while check user in database {e}')
+    return JSONResponse(
+      content={"error": "Database error"},
+      status_code=500
+    )
+  
 @router.get("/get_user/{email}")
 async def get_user(email):
-  result = await get_user_service(email)
-  if result:
-    logger.info("User finded")
-  else:
-    logger.info("User not finded")
-  result["_id"] = str(result["_id"])
-  return result
+  try:
+    result = await get_user_service(email)
+
+    if result:
+      result["_id"] = str(result["_id"])
+      logger.info("User finded")
+      return JSONResponse(
+        content={"message": "User finded",
+                "user": result},
+        status_code=200
+      )
+    else:
+      logger.info("User not found")
+      return JSONResponse(
+        content={"message": "User not found"},
+        status_code=404
+      )
+  except Exception as e:
+    logger.exception(f"Error while searching user: {e}")
+    return JSONResponse(
+      content={"message": "Internal server error"},
+      status_code=500
+    )
