62764c2271836268cc8c8aea0165ff2621a5d092
GuilhermeRodriguesDigi <guilherme.rodrigues@digisystem.com.br>
2025-11-10 17:23:39 -0300

    feature: create and get categories

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from app.routes import user
+from app.routes import user, categories
 
 
 def Create_app():
   app = FastAPI()
 
   app.include_router(user.router)
+  app.include_router(categories.router)
   
   return app
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 from  ..database.schema import User
