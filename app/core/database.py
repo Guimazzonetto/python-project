505111a8575a11e486d10b5278437c374dc593f4
GuilhermeRodriguesDigi <guilherme.rodrigues@digisystem.com.br>
2025-11-11 11:57:34 -0300

    fix: update core.database

--- app/core/database.py
+++ app/core/database.py
@@ -5,11 +5,5 @@ from pymongo import MongoClient
 client = MongoClient(config.MONGO_URI)
 database = client[config.MONGO_DATABASE]
 
-<<<<<<< HEAD
-
-users_collection = database["users"]
-categories_collection = database["categories"]
-=======
 users_collection = database["users"]
 category_collection = database["categories"]
->>>>>>> ad39dc2f0c807e1e4cfebf4f0e2355c521b608e0
